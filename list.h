a50a359b0f0a4937a5c784afdf8aafec2246df85
nik0zer <s21b1.nikozer@gmail.com>
2022-10-22 21:52:13 +0300

    #

--- list.h
+++ list.h
@@ -1,7 +1,19 @@
 #ifndef LIST_H
 #define LIST_H
 
+struct list_node
+{
+    void* node_val;
+    int next_node;
+    int prev_node;
+};
 
+struct list
+{
+    list_node* nodes_arr;
+    int size_of_list;
+    int head_num;
+};
 
 
 
